6fdd35547e6d0bcef012599a77a3a923e0c4d890
Ed Lima <edlima@amazon.com>
2020-07-29 12:19:32 -0700

    Update app.js

--- NodeFunction/hello-world/app.js
+++ NodeFunction/hello-world/app.js
@@ -1,9 +1,7 @@
 let response;
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
         response = "hello world from node"
-                // location: ret.data.trim()"
     } catch (err) {
         console.log(err);
         return err;
